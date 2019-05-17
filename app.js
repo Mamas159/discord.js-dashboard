6143e2695bf70003c04d61228bad3b23e1e588bd
Mamas159 <gevogame@gmail.com>
2019-05-17 01:35:45 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-let port = require('./config.json').port || 3000;
+let port = require('./config.json').port || 8080;
 app.set('port', port);
 
 const session = require('express-session');
